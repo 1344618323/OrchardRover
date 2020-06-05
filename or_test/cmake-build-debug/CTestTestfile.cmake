29f261a8efdceba2e62a72bfb414cc0860a8e0ff
1344618323 <1344618323@qq.com>
2020-06-05 15:17:44 +0800

    fastslam

--- or_test/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cxn/myfile/orchardrover_ws/src/OrchardRover/or_test
-# Build directory: /home/cxn/myfile/orchardrover_ws/src/OrchardRover/or_test/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
