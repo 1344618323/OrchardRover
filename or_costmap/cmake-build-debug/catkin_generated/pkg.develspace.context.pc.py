bf623bb315fecd1e3c89e97548a2a84f42f40239
1344618323 <1344618323@qq.com>
2020-07-03 15:07:33 +0800

    costmap

--- or_costmap/cmake-build-debug/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "or_costmap"
-PROJECT_SPACE_DIR = "/home/cxn/test_ws/src/costmap/or_costmap/cmake-build-debug/devel"
-PROJECT_VERSION = "1.0.0"
