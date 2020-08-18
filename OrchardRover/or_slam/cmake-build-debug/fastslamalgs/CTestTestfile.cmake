8f49b6544b0eb60e25203889038ba023910dfc00
cxn <1344618323@qq.com>
2020-08-18 18:29:39 +0800

    protoc update

--- OrchardRover/or_slam/cmake-build-debug/fastslamalgs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cxn/myfile/OR_ws/orchardrover_ws/src/OrchardRover/or_slam/fastslamalgs
-# Build directory: /home/cxn/myfile/OR_ws/orchardrover_ws/src/OrchardRover/or_slam/cmake-build-debug/fastslamalgs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- OrchardRover/or_test/cmake-build-debug/gtest/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/cxn/myfile/OR_ws/orchardrover_ws/src/OrchardRover/or_test/cmake-build-debug/gtest/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
